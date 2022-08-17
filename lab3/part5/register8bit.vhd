98f60fb560f6d2460ce27ddf6560211b7e063467
Pakorn Sangpeth <camel4882@gmail.com>
2022-08-18 00:31:38 +0700

    test

--- lab3/part5/register8bit.vhd
+++ lab3/part5/register8bit.vhd
@@ -12,7 +12,7 @@ end register8bit;
 
 
 architecture bhv of register8bit is 
-	component d_latch
+	component d_ff
 		port (
 			clk, d  : in std_logic;
 			q : out std_logic
@@ -20,13 +20,13 @@ architecture bhv of register8bit is
 	end component;
 begin
 
-	d0: d_latch port map (En,v(0),h(0)); 
-	d1: d_latch port map (En,v(1),h(1)); 
-	d2: d_latch port map (En,v(2),h(2)); 
-	d3: d_latch port map (En,v(3),h(3)); 
-	d4: d_latch port map (En,v(4),h(4)); 
-	d5: d_latch port map (En,v(5),h(5)); 
-	d6: d_latch port map (En,v(6),h(6)); 
-	d7: d_latch port map (En,v(7),h(7));
+	d0: d_ff port map (En,v(0),h(0)); 
+	d1: d_ff port map (En,v(1),h(1)); 
+	d2: d_ff port map (En,v(2),h(2)); 
+	d3: d_ff port map (En,v(3),h(3)); 
+	d4: d_ff port map (En,v(4),h(4)); 
+	d5: d_ff port map (En,v(5),h(5)); 
+	d6: d_ff port map (En,v(6),h(6)); 
+	d7: d_ff port map (En,v(7),h(7));
 	
 end bhv;
