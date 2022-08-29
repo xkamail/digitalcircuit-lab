ab78c554c9f79abd565e767a3ce1e09dac383e99
Pakorn Sangpeth <camel4882@gmail.com>
2022-08-29 22:25:13 +0700

    prat 4 simulation

--- lab4/part4/simulation/qsim/work/lab4part4/_primary.vhd
+++ lab4/part4/simulation/qsim/work/lab4part4/_primary.vhd
@@ -5,7 +5,6 @@ entity lab4part4 is
         sw              : in     vl_logic_vector(9 downto 0);
         ledr            : out    vl_logic_vector(9 downto 0);
         clock50         : in     vl_logic;
-        debug           : out    vl_logic;
         hex2            : out    vl_logic_vector(0 to 6);
         hex1            : out    vl_logic_vector(0 to 6);
         hex0            : out    vl_logic_vector(0 to 6)
--- lab4/part4/simulation/qsim/work/lab4part4_vlg_check_tst/_primary.vhd
+++ lab4/part4/simulation/qsim/work/lab4part4_vlg_check_tst/_primary.vhd
@@ -2,7 +2,6 @@ library verilog;
 use verilog.vl_types.all;
 entity lab4part4_vlg_check_tst is
     port(
-        debug           : in     vl_logic;
         hex0            : in     vl_logic_vector(0 to 6);
         hex1            : in     vl_logic_vector(0 to 6);
         hex2            : in     vl_logic_vector(0 to 6);
