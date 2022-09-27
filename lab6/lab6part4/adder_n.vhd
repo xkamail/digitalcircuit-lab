9f28d1b7b23e58615d66d1a81c4c78ab88b84e50
Pakorn Sangpeth <camel4882@gmail.com>
2022-09-28 00:33:05 +0700

    fix: adder_n

--- lab6/lab6part4/adder_n.vhd
+++ lab6/lab6part4/adder_n.vhd
@@ -7,26 +7,30 @@ entity adder_n is
 	generic ( n : integer := 8 );
 	port (
 		Cin : in std_logic;
-		a, b : in std_logic_vector(n-1 downto 0);
+		a,b : in std_logic_vector(n-1 downto 0);
+		clk, reset : in std_logic;
 		s : out std_logic_vector(n-1 downto 0);
 		Cout : out std_logic
 	);
 end adder_n;
 	
 architecture bhv of adder_n is
-	component fa is
-		port (
-			c_in, a,b : in std_logic;
-			s,c_out : out std_logic
-		);
-	end component;
-	signal sum : std_logic_vector(n-1 downto 0);
+	signal sum : std_logic_vector(n downto 0);
 	signal c : std_logic_vector(n-1 downto 0);
 begin
-	f0: fa port map (Cin, a(0),b(0),sum(0),c(0));
-	u0: for i in 1 to n-1 generate
-			cell_i: fa port map (c(i-1), a(i),b(i), sum(i), c(i) );
-		end generate;
-	s <= sum;
-	Cout <= c(n-1);
+	process(clk, reset)
+	begin
+		if rising_edge(clk) then
+			if reset = '0' then
+				sum <=  (Others => '0');
+			else
+				sum <= ('0' & A) + ('0' & B) + Cin;
+			end if;
+		else
+			sum <= sum;
+		end if;
+	end process;
+				
+	s <= sum(n-1 downto 0);
+	Cout <= sum(n);
 end bhv;
