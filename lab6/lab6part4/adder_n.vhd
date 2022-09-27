bc3ce45cd1f076657583760fb198cc47075f3be8
Pakorn Sangpeth <camel4882@gmail.com>
2022-09-28 01:04:05 +0700

    fix: adder n

--- lab6/lab6part4/adder_n.vhd
+++ lab6/lab6part4/adder_n.vhd
@@ -5,31 +5,18 @@ use ieee.std_logic_signed.all;
 
 entity adder_n is
 	generic ( n : integer := 8 );
-	port (
-		Cin : in std_logic;
-		a,b : in std_logic_vector(n-1 downto 0);
-		clk, reset : in std_logic;
-		s : out std_logic_vector(n-1 downto 0);
-		Cout : out std_logic
-	);
+		port (
+			Cin : in std_logic;
+			a, b : in std_logic_vector(n-1 downto 0);
+			s : out std_logic_vector(n-1 downto 0);
+			Cout : out std_logic
+		);
 end adder_n;
 	
 architecture bhv of adder_n is
 	signal sum : std_logic_vector(n downto 0);
-	signal c : std_logic_vector(n-1 downto 0);
 begin
-	process(clk, reset)
-	begin
-		if rising_edge(clk) then
-			if reset = '0' then
-				sum <=  (Others => '0');
-			else
-				sum <= ('0' & A) + ('0' & B) + Cin;
-			end if;
-		else
-			sum <= sum;
-		end if;
-	end process;
+	sum <= ('0' & A) + ('0' & B) + Cin;
 				
 	s <= sum(n-1 downto 0);
 	Cout <= sum(n);
