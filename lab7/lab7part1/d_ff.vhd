a83f120b40eb47c8aceb2e05c30e2baa5ad680b2
Pakorn Sangpeth <camel4882@gmail.com>
2022-10-05 01:19:51 +0700

    feat: lab 7 part 1

--- lab7/lab7part1/d_ff.vhd
+++ lab7/lab7part1/d_ff.vhd
@@ -3,7 +3,7 @@ use ieee.std_logic_1164.all;
 
 entity d_ff is
 	port (
-		Clk, D  : in std_logic;
+		Clk, reset, D  : in std_logic;
 		Q : out std_logic
 	);
 end d_ff;
@@ -13,7 +13,11 @@ begin
 	process(d,clk)
 	begin
 		if rising_edge(clk) then
-			q <= d;
+			if reset = '1' then
+				q <= '0';
+			else
+				q <= d;
+			end if;
 		end if;
 	end process;
 end Behavior;
--- lab7/lab7part1/lab7part1.vhd
+++ lab7/lab7part1/lab7part1.vhd
@@ -12,41 +12,26 @@ end lab7part1;
 architecture bhv of lab7part1 is
 	component d_ff is
 		port (
-			Clk, D  : in std_logic;
+			Clk, reset, D  : in std_logic;
 			Q : out std_logic
 		);
 	end component;
 	signal reset, clk, w, z  : std_logic;
 	signal q : std_logic_vector(8 downto 0);
-	signal A,B,C,D,E,F,G,H,I : std_logic;
 begin
 	w <= sw1;
 	clk <= not(key0);
 	reset <= not(sw0); -- active low
-	
-	A <= (not(q(8)) and not(q(7)) and not(q(6)) and not(q(5)) and not(q(4)) and not(q(3)) and not(q(2)) and not(q(1)) and not(q(0)));
-	B <= (not(q(8)) and not(q(7)) and not(q(6)) and not(q(5)) and not(q(4)) and not(q(3)) and not(q(2)) and q(1) and (q(0)));
-	C <= (not(q(8)) and not(q(7)) and not(q(6)) and not(q(5)) and not(q(4)) and not(q(3)) and q(2) and not(q(1)) and (q(0)));
-	D <= (not(q(8)) and not(q(7)) and not(q(6)) and not(q(5)) and not(q(4)) and q(3) and not(q(2)) and not(q(1)) and (q(0)));
-	E <= (not(q(8)) and not(q(7)) and not(q(6)) and not(q(5)) and q(4) and not(q(3)) and not(q(2)) and not(q(1)) and (q(0)));
-	F <= (not(q(8)) and not(q(7)) and not(q(6)) and q(5) and not(q(4)) and not(q(3)) and not(q(2)) and not(q(1)) and (q(0)));
-	G <= (not(q(8)) and not(q(7)) and q(6) and not(q(5)) and not(q(4)) and not(q(3)) and not(q(2)) and not(q(1)) and (q(0)));
-	H <= (not(q(8)) and q(7) and not(q(6)) and not(q(5)) and not(q(4)) and not(q(3)) and not(q(2)) and not(q(1)) and (q(0)));
-	I <=  (q(8) and not(q(7)) and not(q(6)) and not(q(5)) and not(q(4)) and not(q(3)) and not(q(2)) and not(q(1)) and (q(0)));
-	
-	f0: d_ff port map(clk, not(reset) and (A or B or C or D or E or F or G or H or I),                                q(0)); -- A
-	f1: d_ff port map(clk, not(reset) and (not(w) and (A or F or G or H or I)), q(1)); -- B
-	f2: d_ff port map(clk, not(reset) and (not(w) and B), 								 q(2)); -- C
-	f3: d_ff port map(clk, not(reset) and  not(w) and C,                        q(3)); -- D
-	f4: d_ff port map(clk, not(reset) and ((not(w) and E) or (not(w) and D)),     q(4)); -- E/1
-	f5: d_ff port map(clk, not(reset) and w and (A or B or C or D or E),        q(5)); -- F
-	f6: d_ff port map(clk, not(reset) and w and F,                              q(6)); -- G
-	f7: d_ff port map(clk, not(reset) and w and G,                              q(7)); -- H
-	f8: d_ff port map(clk, not(reset) and ((w and I) or( w and H)),               q(8)); -- I/1
-	
-	z <= E or I;
-	
+	f0: d_ff port map(clk, '0', reset, q(0));
+	f1: d_ff port map(clk, reset, not(w) and (q(0) or q(5) or q(6) or q(7) or q(8)), q(1));
+	f2: d_ff port map(clk, reset, not(w) and q(1), q(2));
+	f3: d_ff port map(clk, reset, not(w) and q(2), q(3)); 
+	f4: d_ff port map(clk, reset, (not(w) and q(4)) or (not(w) and q(3)), q(4)); 
+	f5: d_ff port map(clk, reset,  w and (q(0) or q(1) or q(2) or q(3) or q(4)), q(5));
+	f6: d_ff port map(clk, reset,  w and q(5),q(6));
+	f7: d_ff port map(clk, reset,  w and q(6),q(7));
+	f8: d_ff port map(clk, reset,  ((w and q(8)) or( w and q(7))), q(8));
+	z <= q(4) or q(8);
 	ledr(8 downto 0) <= q;
 	ledr(9) <= z;
-	
 end bhv;
