22c344ca495c16dbb678e796715026c20eb45710
Pakorn Sangpeth <camel4882@gmail.com>
2022-10-09 22:24:35 +0700

    delete lab8

--- lab8_old/lab8part1/simulation/qsim/work/lab8part1_vlg_check_tst/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity lab8part1_vlg_check_tst is
-    port(
-        v               : in     vl_logic_vector(3 downto 0);
-        sampler_rx      : in     vl_logic
-    );
-end lab8part1_vlg_check_tst;
--- lab8_old/lab8part3/simulation/qsim/work/lab8part3_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity lab8part3_vlg_vec_tst is
-end lab8part3_vlg_vec_tst;
